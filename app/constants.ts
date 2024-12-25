80ff9a63e06a89334af0fbfa9eb3c1fc82b7063c
Jan-Gabriel <jan-gabriel.mylius@quality-match.com>
2024-12-26 00:15:02 +0100

    adjust constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,8 +1,8 @@
-export const ANLAUTE = ['M', 'L', 'I', 'O', 'A', 'U', 'R', 'E', 'N', 'T', 'S', 'EI', 'AU', 'EU', 'H', 'W', 'F', 'CH', 'SCH', 'B', 'D', 'G', 'K', 'P', 'Z', 'J', 'V', 'Ä', 'Ö', 'Ü', 'QU', 'X', 'Y'];
+export const ANLAUTE = ['M', 'L', 'I', 'O', 'A', 'U', 'R', 'E', 'N', 'T', 'S', 'J', 'EI', 'AU', 'EU', 'H', 'W', 'F', 'CH', 'SCH', 'B', 'D', 'G', 'K', 'P', 'Z', 'V', 'Ä', 'Ö', 'Ü', 'QU', 'X', 'Y'];
 
 export const WORD_BANK = {
     subjects: ['Mama', 'Papa', 'Oma', 'Opa', 'Tim','Tarzan', 'Lisa', 'Die Katze', 'Der Hund', 'Anna','Der Vogel','Der Bär', 'Der Tiger', 'Die Qualle','Das Zebra','Jonas','Tom','Lili','Linda','Maria','Der Fuchs','Der Hase','Der Igel','Der Elefant','Der Affe','Der Löwe','Der Biber','Der Hirsch','Der Hahn','Der Hase','Der Hirsch','Der Hund','Der Igel','Der Käfer','Der Kater','Der Käfer','Der Käse','Der Käfer'],
-    predicates: ['liest', 'malt', 'spielt', 'läuft', 'sitzt','sammelt','stemmt','rennt','schläft','schreibt','lacht','tanzt','singt','fliegt','jubelt'],
+    predicates: ['liest', 'malt', 'spielt', 'läuft', 'sitzt','sammelt','stemmt','rennt','schläft','schreibt','lacht','tanzt','singt','fliegt','jubelt','jagt'],
     objects: ['im Haus', 'im Garten', 'am Tisch', 'mit mir', 'hier', 'dort', 'im Bett', 'im Auto', 'im Park', 'im Wald', 'im Zoo', 'am Strand', 'im Bus', 'matt', 'am See','im Baum','im Zug','im Boot','hier','mit dir','im Tal', 'mit Oma', 'mit Opa', 'mit Tim', 'im Ort','mit Mama','im Laden','am Bach', 'im Wald', 'in der Wüste', 'am Feld', 'im Feld', 'im Stall', 'im Schnee', 'im Haus','im Laden','am Bach', 'im Wald', 'in der Wüste', 'am Feld', 'im Feld', 'im Stall', 'im Schnee', 'im Haus','im Keller', 'im Stall', 'im Garten', 'im Laub'],
     adjectives: ['müde', 'klein', 'groß', 'nett', 'laut', 'leise', 'alt', 'neu', 'blond', 'schlau', 'stark', 'satt', 'nass', 'ernst', 'matt','jung'],
 };
